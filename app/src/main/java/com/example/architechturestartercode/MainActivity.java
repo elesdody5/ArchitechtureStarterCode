20f8ce5a82b6acfdd752654621d2e204f200c5dc
AhmedELESDODYyassir <ahmed.elesdody@yassir.com>
2025-12-22 16:06:28 +0200

    starter code

--- app/src/main/java/com/example/architechturestartercode/MainActivity.java
+++ app/src/main/java/com/example/architechturestartercode/MainActivity.java
@@ -1,5 +1,6 @@
-package com.example.roomdemo;
+package com.example.architechturestartercode;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
